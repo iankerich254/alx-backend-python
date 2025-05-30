f7a2fc1226842c10d5e9c41654f5b57005a88e08
Ian Kerich <iankerich254@gmail.com>
2025-05-30 21:11:47 +0300

    Adding the project's README.md file and test_utils.py file.

---
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -0,0 +1,3 @@
+## Unittests and Integration Tests
+
+This README file is meant for the project on unittests and integration tests.
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,20 @@
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+#!/usr/bin/env python3
+"""Unit tests for utils.py module.
+"""
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test cases for access_nested_map function."""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test access_nested_map returns expected result."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
