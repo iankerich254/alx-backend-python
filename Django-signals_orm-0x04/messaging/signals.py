bca1643b26e74fdbb353eaae7c739f922ce3a7e3
Ian Kerich <iankerich254@gmail.com>
2025-06-15 14:06:01 +0300

    Use signals for deleting user-related data

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,10 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from .models import Message, Notification, MessageHistory
+from django.db.models import Q
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
@@ -14,3 +18,9 @@ def log_message_edit(sender, instance, **kwargs):
         if old_msg.content != instance.content:
             MessageHistory.objects.create(message=instance, old_content=old_msg.content)
             instance.edited = True
+
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    Message.objects.filter(Q(sender=instance) | Q(receiver=instance)).delete()
+    Notification.objects.filter(user=instance).delete()
+    MessageHistory.objects.filter(message__sender=instance).delete()
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def delete_user(request):
+    request.user.delete()
+    return redirect('logout')
